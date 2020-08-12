9d97ee7dbff3b760cb8cb5d7474d35a3bb649eaa
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-08-12 13:01:23 +0200

    resent blog, man kan ikke gå videre til enkel visning endnu

---
+++ src/js/resent-blog.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    let recentBlog = document.querySelector(".recent-blog-insert");
+
+    fetch("http://localhost:4000/blog-posts", {
+        "method": "GET",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then (function(response){
+        return response.json();
+    })
+    .then (function(result){
+        console.log(result);
+        result.forEach(blog => {
+            let contentSmaller = blog.content.slice(0, 200);
+            recentBlog.innerHTML+=`
+                <div>
+                    <img src="./assets/images/content-img/blog_full` + blog.id + `.jpg" alt=""/>
+                    <h3>` + blog.title + `</h3>
+                    <p class="pink">` + blog.author + `</p>
+                    <p>` + contentSmaller + "..." + `</p>
+                </div>
+            `;
+        });
+    });
+});
