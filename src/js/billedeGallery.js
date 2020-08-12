ba15a41508d4b2bf95552a0589c58ae0010ed1ae
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-08-12 10:42:25 +0200

    sektion 3 gallery, hentet fra api men det er ikke interactivt endnu

---
+++ src/js/billedeGallery.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", ()=>{ 
+    let insertImgs = document.querySelector(".billede-gallery");
+    let insertImgs2 = document.querySelector(".billede-gallery2");
+
+    fetch("http://localhost:4000/assets", {
+        "method": "GET",
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then (function(response){
+        return response.json();
+    })
+    .then (function(result){
+        console.log(result)
+        result.forEach(img => {
+            if(img.id < 6 ){
+                insertImgs.innerHTML+=`
+                    <img src="` + img.url + `" alt="` + img.name + `"/>
+                `;
+            }else if(img.id > 7 && img.id < 15){
+                insertImgs2.innerHTML+=`
+                    <img src="` + img.url + `" alt="` + img.name + `"/>
+                `;
+                console.log(img.id)
+            }
+        });
+    });
+});
