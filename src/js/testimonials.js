3e5e17c713a419cbd822ad26c9a9b05ca3b42a9c
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-08-13 11:30:44 +0200

    ordnede stressfejl

--- src/js/testimonials.js
+++ src/js/testimonials.js
@@ -8,24 +8,24 @@ document.addEventListener("DOMContentLoaded", ()=>{
         box1.classList.add("pink");
         box2.classList.remove("pink");
         box3.classList.remove("pink");
-        fetcher(1);
+        fetcher(0);
     });
 
     box2.addEventListener("click", ()=>{
         box1.classList.remove("pink");
         box2.classList.add("pink");
         box3.classList.remove("pink");
-        fetcher(2);
+        fetcher(1);
     });
 
     box3.addEventListener("click", ()=>{
         box1.classList.remove("pink");
         box2.classList.remove("pink");
         box3.classList.add("pink");
-        fetcher(3);
+        fetcher(2);
     });
 
-    fetcher(1);
+    fetcher(0);
 
     function fetcher(index){
         fetch("http://localhost:4000/testemonials", {
@@ -38,20 +38,18 @@ document.addEventListener("DOMContentLoaded", ()=>{
             return response.json();
         })
         .then (function(result){
-            result.forEach(get => {
-                if(get.id === index){
-                    testimonials.innerHTML=`
-                        <img src="./assets/images/content-img/testimonial_` + get.id + `.jpg" alt="` + get.name + `">
-                        <h4>` + get.name + `</h4>
-                        <p>` + get.content + `</p>
-                        <ul>
-                            <li><a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
-                            <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="https://myaccount.google.com/"><i class="fab fa-google-plus-g"></i></a></li>
-                        </ul>
-                    `;
-                }
-            });
+            let index2 = index + 1;
+            testimonials.innerHTML=`
+                <img src="./assets/images/content-img/testimonial_` + index2 + `.jpg" alt="` + result[index].name + `">
+                <h4>` + result[index].name + `</h4>
+                <p>` + result[index].content + `</p>
+                <ul>
+                <li><a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
+                <li><a href="https://twitter.com/"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="https://myaccount.google.com/"><i class="fab fa-google-plus-g"></i></a></li>
+                </ul>
+            `;
+
         });
     }
 });
