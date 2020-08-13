45496d3db1fb7fdabd41a26d83b44adfcac825dd
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-08-13 10:37:00 +0200

    ordnede en fejl men det virker stadigvæk ikke

--- src/js/newsletter-post.js
+++ src/js/newsletter-post.js
@@ -2,19 +2,18 @@
 document.querySelector("#button").addEventListener("click", ()=>{
     let email = document.querySelector("#newsletter-email").value;
     console.log(email);
-    post(email.value);
+    post(email);
 
 })
 function post(theEmail){
     fetch("http://localhost:4000/newsletters", {
         method: "POST",
         headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
+            "Content-Type": "application/x-www-form-urlencoded"
         },
         body: JSON.stringify({
-            email: theEmail
+            email:theEmail
         })
     })
     .catch(error => console.error(error))
 }
-// javascript:alert('Thank you for subscribing to our newsletter!')
